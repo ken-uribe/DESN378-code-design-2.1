681b41e96a60049de39abbf47f5fcba9752319c8
Kenia <kuribe@ewu.edu>
2026-01-12 03:26:14 +0000

    test3: image changing

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,16 @@ console.log('Portfolio loaded');
 
 // Week 0: Hello World!
 //alert("Hello World!" );
+const myImage = document.querySelector("img");
 
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
 // Week 3+: More to come...
