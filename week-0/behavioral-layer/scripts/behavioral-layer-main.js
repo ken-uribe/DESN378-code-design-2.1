71e89cd64f78415c9236d46f97746a9f706e5b63
Kenia <kuribe@ewu.edu>
2026-01-12 04:33:19 +0000

    updated images

--- week-0/behavioral-layer/scripts/behavioral-layer-main.js
+++ week-0/behavioral-layer/scripts/behavioral-layer-main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
   const myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  myHeading.textContent = `The Greatest, ${myName}`;
 }
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `The Greatest, ${storedName}`;
 }
 
 myButton.addEventListener("click", () => {
