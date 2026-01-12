836ff4af7f93d69408a3fce80a64a6302f9378cb
Kenia <kuribe@ewu.edu>
2026-01-12 03:19:15 +0000

    updated alert in scripts/main.js to be a comment so it doesn't pop-up

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@
 console.log('Portfolio loaded');
 
 // Week 0: Hello World!
-alert("Hello World!" );
+//alert("Hello World!" );
 
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
