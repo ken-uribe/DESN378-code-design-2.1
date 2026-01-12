9ebfc535e68053eca093fc882f319b84e9c25b70
Kenia <kuribe@ewu.edu>
2026-01-12 03:51:26 +0000

    added more JS to behavioral layer main JS

--- week-0/behavioral-layer/scripts/behavioral-layer-main.js
+++ week-0/behavioral-layer/scripts/behavioral-layer-main.js
@@ -1,16 +1,31 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
-
 const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/fox1.png") {
+  if (mySrc === "images/fox11.png") {
     myImage.setAttribute("src", "images/fox2.png");
   } else {
     myImage.setAttribute("src", "images/fox1.png");
   }
-});let myButton = document.querySelector("button");
+});
+
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
