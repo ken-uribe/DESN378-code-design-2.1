475e580093cb9cbcebed789faeca25ec49bfda53
Kenia <kuribe@ewu.edu>
2026-01-12 03:04:56 +0000

    hopefully linked scripts

--- week-0/behavioral-layer/scripts/main.js
+++ week-0/behavioral-layer/scripts/main.js
@@ -2,3 +2,14 @@
 const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
