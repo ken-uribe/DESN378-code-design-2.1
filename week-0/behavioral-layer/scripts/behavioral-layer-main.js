280dd8ac5c886e99130d81f43292d013018af91a
Kenia <kuribe@ewu.edu>
2026-01-12 03:42:13 +0000

    Success: Was able to make the image change back and forth

--- week-0/behavioral-layer/scripts/behavioral-layer-main.js
+++ week-0/behavioral-layer/scripts/behavioral-layer-main.js
@@ -10,6 +10,6 @@ myImage.addEventListener("click", () => {
   if (mySrc === "images/fox1.png") {
     myImage.setAttribute("src", "images/fox2.png");
   } else {
-    myImage.setAttribute("src", "images/fox-1.png");
+    myImage.setAttribute("src", "images/fox1.png");
   }
 });
